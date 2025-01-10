d8f728aa93589fb6d8fea455054121b55259bca5
Zaman-Pallob <atikurzamanpallob@gmail.com>
2025-01-10 18:18:06 +0600

    database added

---
+++ lib/app_database/app_database.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqlite3/sqlite3.dart';
+import 'package:path/path.dart' as p;
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
+part 'app_database.g.dart';
+
+@DriftDatabase(tables: [], daos: [])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'ezy_course_app.sqlite'));
+    if (!await file.exists()) {
+      if (Platform.isAndroid) {
+        await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
+      }
+      final cachebase = (await getTemporaryDirectory()).path;
+      sqlite3.tempDirectory = cachebase;
+      return NativeDatabase.createInBackground(file);
+    } else {
+      return NativeDatabase(file);
+    }
+  });
+}
